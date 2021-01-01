62651f9a0d107e2da234a8eb674914ac1f182ef2
Himanshu-Kandwal <hkandwal50@gmail.com>
2021-01-01 23:52:49 +0530

    Data Binding enabled

--- app/src/main/java/com/example/databindingsample/MainActivity.java
+++ app/src/main/java/com/example/databindingsample/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.databindingsample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.example.databindingsample.databinding.ActivityMainBinding;
+import com.example.databindingsample.models.User;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        //set content view using binding
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        User user;
+        user= new User();
+        user.setName("Himanshu");
+        user.setEmail("himanshu@gmail.com");
+
+        activityMainBinding.setUser(user);
+
     }
 }
